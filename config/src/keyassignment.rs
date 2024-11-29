9aeb02cb73ee08f9ccb26759051d73ed40e43396
René Weselowski <rene.weselowski@gmail.com>
2024-11-29 22:40:25 +0100

    adds movement for start of word, regardless of stop characters
    Sample test/with-characters that cause stopps
                        -(cursor)
           -(after hitting MoveBackwardWordStart)

--- config/src/keyassignment.rs
+++ config/src/keyassignment.rs
@@ -693,6 +693,7 @@ pub enum CopyModeAssignment {
     MoveToStartOfNextLine,
     MoveToSelectionOtherEnd,
     MoveToSelectionOtherEndHoriz,
+    MoveBackwardWordStart,
     MoveBackwardWord,
     MoveForwardWord,
     MoveForwardWordEnd,
