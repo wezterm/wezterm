0045c393629d7c5dc96b2f416a9af38f193efb34
mgpinf <manishgowd09@gmail.com>
2025-08-11 01:04:16 +0530

    refactor: elide lifetime annotation for reference to TransientOption
    Elide lifetime annotation for reference to TransientOption parameter in
    line_prompt_method in TransientMenu

--- wezterm-gui/src/overlay/transient.rs
+++ wezterm-gui/src/overlay/transient.rs
@@ -710,7 +710,7 @@ impl<'a> TransientState<'a> {
     fn line_prompt(
         &mut self,
         term: &mut TermWizTerminal,
-        option: &'a TransientOption<'a>,
+        option: &TransientOption<'a>,
     ) -> anyhow::Result<()> {
         let size = term.get_screen_size()?;
         self.changes.append(&mut vec![
