a90ef086dd93a099dc57dbe9e2d12daf68b79dc6
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-09-01 00:49:11 +0200

    Revert "Fix crash on hidpi with wayland (#6508)"
    This reverts commit d1684d38e83d31dcd28dfc7dcc36ac4ba9eb582c.

--- window/src/os/wayland/window.rs
+++ window/src/os/wayland/window.rs
@@ -921,8 +921,6 @@ impl WaylandWindowInner {
                         ) {
                             self.surface().attach(Some(buffer.wl_buffer()), 0, 0);
                             self.surface().set_buffer_scale(factor as i32);
-                            self.surface().commit();
-
                             self.surface_factor = factor;
                         }
                     }
