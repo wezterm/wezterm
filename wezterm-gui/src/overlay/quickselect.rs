e7fc45b0b37478f3299cf71241144f62cc3a6fd2
俺不叫化咕龙 <1801943622@qq.com>
2026-07-01 19:52:33 +0800

    Optimize case conversion performance

--- wezterm-gui/src/overlay/quickselect.rs
+++ wezterm-gui/src/overlay/quickselect.rs
@@ -76,7 +76,7 @@ fn compute_labels_for_alphabet_impl(
     let alphabet = if make_lowercase {
         alphabet
             .chars()
-            .map(|c| c.to_lowercase().to_string())
+            .map(|c| c.to_lowercase())
             .collect::<Vec<String>>()
     } else {
         alphabet
